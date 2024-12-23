195812b3e7df87dbc45251e222a73f261d08f77b
frhanim13 <48370531+frhanim13@users.noreply.github.com>
2024-12-23 10:07:56 +0800

    Update code.sql

--- Analyze International Debt Statistics/code.sql
+++ Analyze International Debt Statistics/code.sql
@@ -1,1 +1,36 @@
 
+-- num_distinct_countries 
+select 
+count(distinct country_name) as  total_distinct_countries
+
+from international_debt
+
+-- highest_debt_country 
+
+select 
+country_name,
+sum(debt) as total_debt
+
+from international_debt
+group by country_name
+order by total_debt desc
+limit 5
+
+-- lowestt_debt_country 
+
+select 
+country_name,
+sum(debt) as total_debt
+
+from international_debt
+group by country_name
+order by total_debt asc
+limit 5
+
+
+
+
+
+
+
+
